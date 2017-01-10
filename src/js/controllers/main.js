aacb378b9702873a8bd07fa3c13d84a2593d3f5f
Elliot Brock <ell_304@msn.com>
2017-01-10 17:07:51 +0000

    added main controller, usersShow controller

---
+++ src/js/controllers/main.js
@@ -0,0 +1,35 @@
+angular.module('artHub')
+.controller('MainController', MainController);
+
+MainController.$inject = ['$auth', '$state', '$rootScope'];
+function MainController($auth, $state, $rootScope) {
+  const main = this;
+
+  main.isLoggedIn = $auth.isAuthenticated;
+  main.message = null;
+
+
+  function logout() {
+    $auth.logout()
+    .then(() => {
+      $state.go('usersIndex');
+    });
+  }
+
+  const protectedStates = ['usersEdit'];
+
+  function protectStates(e, toState) {
+    const payload = $auth.getPayload();
+    if(payload) {
+      main.isCurrentUser = $auth.getPayload().id;
+    }
+    if(!$auth.isAuthenticated() && protectedStates.includes(toState.name)) {
+      e.preventDefault();
+      $state.go('login');
+    }
+  }
+
+  $rootScope.$on('$stateChangeStart', protectStates);
+
+  main.logout = logout;
+}
--- src/js/controllers/users.js
+++ src/js/controllers/users.js
@@ -1,9 +1,26 @@
 angular.module('artHub')
-  .controller('UsersIndexController', UsersIndexController);
+  .controller('UsersIndexController', UsersIndexController)
+  .controller('UsersShowController', UsersShowController);
+
 
 UsersIndexController.$inject = ['User'];
 function UsersIndexController(User) {
   const usersIndex = this;
-
   usersIndex.all = User.query();
+}
+
+
+UsersShowController.$inject = ['User', '$state', '$auth'];
+function UsersShowController(User, $state, $auth) {
+  const usersShow = this;
+  usersShow.isLoggedIn = $auth.isAuthenticated;
+  usersShow.user = User.get($state.params);
+
+  function deleteUser() {
+    usersShow.user.$remove(() => {
+      $state.go('usersIndex');
+    });
+  }
+
+  usersShow.deleteUser = deleteUser;
 }
